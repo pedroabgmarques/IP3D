41f445581ff8a1f87053dfc3c2d7988cffc388d6
pedroabgmarques <pedroabgmarques@gmail.com>
2015-09-22 19:20:40 +0100

    Classe estática Geometry

--- Vertex/Game1.cs
+++ Vertex/Game1.cs
@@ -12,17 +12,8 @@ namespace Vertex
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
-        //Array de VertexPositionColor para guardar a lista de vértices e respetivas cores
-        VertexPositionColor[] vertexList;
-
-        //Necessário para desenhar as primitivas
-        VertexBuffer vertexBuffer;
-
         //Matrizes World, View e Projection
         Matrix World, View, Projection;
-
-        //Shader utilizado na renderização
-        BasicEffect basicEffect;
 
         public Game1()
         {
@@ -38,9 +29,7 @@ namespace Vertex
         /// </summary>
         protected override void Initialize()
         {
-            //3 vértices, 1 triângulo
-            vertexList = new VertexPositionColor[3];
-
+            
             //Inicializar as matrizes world, view e projection
             World = Matrix.CreateTranslation(0, 0, 0);
             View = Matrix.CreateLookAt(new Vector3(0, 0, 3), new Vector3(0, 0, 0), new Vector3(0, 1, 0));
@@ -48,6 +37,11 @@ namespace Vertex
                 GraphicsDevice.Viewport.Width / GraphicsDevice.Viewport.Height, 
                 0.01f, 
                 100f);
+
+            //Criar e definir o resterizerState a utilizar para desenhar a geometria
+            RasterizerState rasterizerState = new RasterizerState();
+            rasterizerState.CullMode = CullMode.None;
+            GraphicsDevice.RasterizerState = rasterizerState;
 
             base.Initialize();
         }
@@ -61,15 +55,9 @@ namespace Vertex
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            //Criar um basicEffect que vamos usar para renderizar o triângulo
-            basicEffect = new BasicEffect(GraphicsDevice);
+            //Carregar o efeito a utilizar para desenhar a geometria
+            Geometry.LoadContent(GraphicsDevice);
 
-            vertexList[0] = new VertexPositionColor(new Vector3(0, 1, 0), Color.Red);
-            vertexList[1] = new VertexPositionColor(new Vector3(1, -1, 0), Color.Green);
-            vertexList[2] = new VertexPositionColor(new Vector3(-1, -1, 0), Color.Blue);
-
-            vertexBuffer = new VertexBuffer(GraphicsDevice, typeof(VertexPositionColor), vertexList.GetLength(0), BufferUsage.WriteOnly);
-            vertexBuffer.SetData<VertexPositionColor>(vertexList);
         }
 
         /// <summary>
@@ -104,23 +92,8 @@ namespace Vertex
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            //Desenhar o triângulo
-            basicEffect.World = World;
-            basicEffect.View = View;
-            basicEffect.Projection = Projection;
-            basicEffect.VertexColorEnabled = true;
-
-            GraphicsDevice.SetVertexBuffer(vertexBuffer);
-
-            RasterizerState rasterizerState = new RasterizerState();
-            rasterizerState.CullMode = CullMode.None;
-            GraphicsDevice.RasterizerState = rasterizerState;
-
-            foreach (EffectPass pass in basicEffect.CurrentTechnique.Passes)
-            {
-                pass.Apply();
-                GraphicsDevice.DrawPrimitives(PrimitiveType.TriangleList, 0, 1);
-            }
+            //Desenhar a geometria
+            Geometry.Draw(World, View, Projection, GraphicsDevice);
 
             base.Draw(gameTime);
         }
---
+++ Vertex/Geometry.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Vertex
+{
+    static class Geometry
+    {
+
+        /// <summary>
+        /// Array para guardar os vértices e respetivas cores
+        /// </summary>
+        static private VertexPositionColor[] vertexList;
+
+        /// <summary>
+        /// Buffer de vértices a desenhar
+        /// </summary>
+        static private VertexBuffer vertexBuffer;
+
+        static private BasicEffect efeito;
+        /// <summary>
+        /// Efeito a utilizar para desenhar a geometria
+        /// </summary>
+        static public BasicEffect Efeito
+        {
+            get { return efeito; }
+        }
+        
+
+        /// <summary>
+        /// Define a geometria a ser desenhada
+        /// </summary>
+        /// <param name="graphics">Instância de GraphicsDevice</param>
+        /// <returns></returns>
+        static public VertexBuffer Create3DGeometry(GraphicsDevice graphics)
+        {
+            //3 vértices, 1 triângulo
+            vertexList = new VertexPositionColor[3];
+
+            //Definir os vértices
+            vertexList[0] = new VertexPositionColor(new Vector3(0, 1, 0), Color.Red);
+            vertexList[1] = new VertexPositionColor(new Vector3(1, -1, 0), Color.Green);
+            vertexList[2] = new VertexPositionColor(new Vector3(-1, -1, 0), Color.Blue);
+
+            //Inserir os vértices no buffer
+            vertexBuffer = new VertexBuffer(graphics, typeof(VertexPositionColor), vertexList.GetLength(0), BufferUsage.WriteOnly);
+            vertexBuffer.SetData<VertexPositionColor>(vertexList);
+
+            return vertexBuffer;
+        }
+
+        /// <summary>
+        /// Define o efeito (shader / HLSL) a usar para desenhar a cena
+        /// </summary>
+        /// <param name="graphics"></param>
+        static public void LoadContent(GraphicsDevice graphics)
+        {
+            //Criar um basicEffect que vamos usar para renderizar o triângulo
+            efeito = new BasicEffect(graphics);
+        }
+
+        static public void Draw(Matrix World, Matrix View, Matrix Projection, GraphicsDevice graphics)
+        {
+            efeito.World = World;
+            efeito.View = View;
+            efeito.Projection = Projection;
+            efeito.VertexColorEnabled = true;
+
+            graphics.SetVertexBuffer(Create3DGeometry(graphics));
+
+            foreach (EffectPass pass in efeito.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+                graphics.DrawPrimitives(PrimitiveType.TriangleList, 0, 1);
+            }
+        }
+
+    }
+}
